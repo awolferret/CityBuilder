8ed78f8fc5eed5c6f18d3ab9eaabc0512344d531
awolferret <104518827+awolferret@users.noreply.github.com>
2024-07-07 16:57:14 +0300

    assetAddedAndOptimized

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioPlayer : MonoBehaviour
+    {
+        public AudioClip placementSound;
+        public AudioSource audioSource;
+
+        public static AudioPlayer instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(this.gameObject);
+        }
+
+        public void PlayPlacementSound()
+        {
+            if(placementSound != null) 
+                audioSource.PlayOneShot(placementSound);
+        }
+    }
+}
